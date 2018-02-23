9e98c6ed6f6ae1f3cf900b4972eebb1dc493ee99
Sean Newman <seangnewman@msn.com>
2018-02-23 06:15:50 -0700

    Fixed mixed content error

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 
   $('#quoteButton').click(function(){
-      $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
+      $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
          .done(updateQuoteContainer)
          .fail(handleErr);
   });
